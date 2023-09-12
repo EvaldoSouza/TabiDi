c47c7835bef0e64e654c9421e2bedd986fb69120
glbarreto <gabrielbarreto180@gmail.com>
2023-09-12 09:13:30 -0300

    interface

---
+++ interface_login/futdata.py
@@ -0,0 +1,38 @@
+# No arquivo futdata.py
+
+import sqlite3
+
+class Datafut:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.create_table()
+
+    def create_table(self):
+        cursor = self.conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                          (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                           username TEXT UNIQUE NOT NULL,
+                           password TEXT NOT NULL)''')
+        self.conn.commit()
+
+    def check_credentials(self, username, password):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user = cursor.fetchone()
+        if user:
+            return True
+        else:
+            return False
+
+    def register_user(self, username, password):
+        cursor = self.conn.cursor()
+        try:
+            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            self.conn.commit()
+            return True
+        except sqlite3.IntegrityError:
+            # Usuário com mesmo nome já existe
+            return False
+
+    def close(self):
+        self.conn.close()
