5adfbb7ed5515aea540e7d7f52f882949b2b8532
João Antônio <joaoasccc@gmail.com>
2023-11-02 10:16:23 -0300

    +tela_camp
    agora da pra ver a classificação do campeonato

--- View/tela_camp.py
+++ View/tela_camp.py
@@ -3,58 +3,40 @@ from tkinter import ttk
 import tkinter as tk
 from tkinter import ttk
 
+import tkinter as tk
+from tkinter import ttk
+
 class Tela_Classificacao(tk.Toplevel):
-    def __init__(self, controller, classificacao):
-        super().__init__(controller)
-        self.controller = controller
+    def __init__(self, classificacao_campeonato):
+        super().__init__()
         self.geometry("900x600")
         self.resizable(width="TRUE", height="TRUE")
         self.title("Classificação do Campeonato")
 
-        # Barra de pesquisa
-        self.pesquisa_label = tk.Label(self, text="Pesquisar Time:", bg="#F0F0F0", font=("Arial", 12))
-        self.time_pesquisado_var = tk.StringVar()
-        self.entry_time_pesquisado = tk.Entry(self, font=("Arial", 14), textvariable=self.time_pesquisado_var)
-        self.pesquisa_label.place(relx=0.1, rely=0.1, anchor="w")
-        self.entry_time_pesquisado.place(relx=0.3, rely=0.1, anchor="w")
+        # Cria a tabela
+        self.construir_tabela_campeonatos(classificacao_campeonato)
 
-        self.entry_time_pesquisado.bind('<Return>', self.pesquisar_time)
-
-        # Tabela de Classificação
-        colunas = ("Nome", "Pontos", "Vitórias", "Derrotas", "Empates")
-        self.tabela_classificacao = ttk.Treeview(self, columns=colunas, show="headings")
-        for coluna in colunas:
-            self.tabela_classificacao.heading(coluna, text=coluna)
-            self.tabela_classificacao.column(coluna, width=100)  # Ajuste a largura conforme necessário
-
-        self.construir_classificacao(classificacao)
-
-        self.tabela_classificacao.bind('<<TreeviewSelect>>', self.time_selecionado_evento)
-
-        # Botões
-        self.pesquisar_button = tk.Button(self, text="Pesquisar", command=self.pesquisar_time, bg="blue", fg="white", font=("Arial", 14))
-        self.pesquisar_button.place(relx=0.9, rely=0.1, anchor="se")
-
+        # Botão de voltar
         self.voltar_button = tk.Button(self, text="Voltar", command=self.voltar, bg="yellow", fg="black", font=("Arial", 14))
-        self.voltar_button.place(relx=0.9, rely=0.3, anchor="se")
+        self.voltar_button.place(relx=0.9, rely=0.1, anchor="se")
 
-    def construir_classificacao(self, classificacao):
-        for time in classificacao:
+    def construir_tabela_campeonatos(self, classificacao_campeonato):
+        colunas_campeonatos = ("Nome", "Pontos", "Vitórias", "Derrotas", "Empates")
+        self.tabela_campeonatos = ttk.Treeview(self, columns=colunas_campeonatos, show='headings')
+
+        for coluna in colunas_campeonatos:
+            self.tabela_campeonatos.heading(coluna, text=coluna)
+            self.tabela_campeonatos.column(coluna, width=100)  # Ajuste a largura conforme necessário
+
+        for time in classificacao_campeonato:
             nome = time["nome"]
             pontos = time["pontos"]
             vitorias = time["vitorias"]
             derrotas = time["derrotas"]
             empates = time["empates"]
-            self.tabela_classificacao.insert("", "end", values=(nome, pontos, vitorias, derrotas, empates))
+            self.tabela_campeonatos.insert("", "end", values=(nome, pontos, vitorias, derrotas, empates))
 
-    def pesquisar_time(self, event):
-        parametro = self.time_pesquisado_var.get()
-        # Implemente a lógica para pesquisar um time na classificação do campeonato
-        pass
-
-    def time_selecionado_evento(self, event):
-        # Implemente a lógica para lidar com a seleção de um time na classificação
-        pass
+        self.tabela_campeonatos.pack()
 
     def voltar(self):
         self.destroy()
--- View/tela_lista_camps.py
+++ View/tela_lista_camps.py
@@ -8,13 +8,14 @@ lista_campeonatos = [
     {"nome": "Campeonato 1", "descricao": "Descrição do Campeonato 1"},
     {"nome": "Campeonato 2", "descricao": "Descrição do Campeonato 2"},
 ]
-classificacao = [
+classificacao_campeonato = [
     {"nome": "Time A", "pontos": 12, "vitorias": 4, "derrotas": 2, "empates": 0},
     {"nome": "Time B", "pontos": 10, "vitorias": 3, "derrotas": 1, "empates": 1},
     {"nome": "Time C", "pontos": 8, "vitorias": 2, "derrotas": 2, "empates": 2},
     {"nome": "Time D", "pontos": 6, "vitorias": 1, "derrotas": 3, "empates": 3},
     {"nome": "Time E", "pontos": 4, "vitorias": 0, "derrotas": 4, "empates": 4},
 ]
+
 class Tela_Campeonatos(tk.Toplevel):
     def __init__(self, controller, lista_campeonatos):
         super().__init__()
@@ -22,6 +23,8 @@ class Tela_Campeonatos(tk.Toplevel):
         self.geometry("900x600")
         self.resizable(width="TRUE", height="TRUE")
         self.title("Lista de Campeonatos")
+
+        self.tabela_campeonatos = None
 
         self.background_image = PhotoImage(file="View/img/football_background.png")
         self.background_label = tk.Label(self, image=self.background_image)
@@ -36,15 +39,6 @@ class Tela_Campeonatos(tk.Toplevel):
         self.voltar_button = tk.Button(self, text="Voltar", command=self.voltar, bg="yellow", fg="black", font=("Arial", 14))
         self.voltar_button.place(relx=0.9, rely=0.3, anchor="se")
 
-    def construir_lista_campeonatos(self, lista_campeonatos):
-        # Implemente a construção da lista de campeonatos aqui, com base em 'lista_campeonatos'
-        # Use Treeview ou outra estrutura adequada
-
-        pass
-
-    def ranking(self):
-        tela_classificacao = Tela_Classificacao(self.controller)
-        tela_classificacao.mainloop()
 
     def voltar(self):
         self.destroy()
@@ -68,28 +62,32 @@ class Tela_Campeonatos(tk.Toplevel):
         lista_scrollbar.config(command=lista_campeonatos_listbox.yview)
 
 
-    def construir_tabela_campeonatos(self, lista_campeonatos):
+    def construir_tabela_campeonatos(self, classificacao_campeonato):
         try:
             self.tabela_campeonatos.destroy()
         except AttributeError:
             print("Criando nova tabela de campeonatos?")
 
-        colunas_campeonatos = ("nome", "descricao")
+        colunas_campeonatos = ("nome", "pontos", "vitorias", "derrotas", "empates")
         self.tabela_campeonatos = ttk.Treeview(self, columns=colunas_campeonatos, show='headings')
-        self.tabela_campeonatos.heading("nome", text="Nome")
-        self.tabela_campeonatos.heading("descricao", text="Descrição")
+        
+        for coluna in colunas_campeonatos:
+            self.tabela_campeonatos.heading(coluna, text=coluna.capitalize())  # Use o nome da coluna como título
 
-        for campeonato in lista_campeonatos:
-            self.inserir_item(self.tabela_campeonatos, campeonato)
+        for time in classificacao_campeonato:
+            self.tabela_campeonatos.insert('', 'end', values=(time["nome"], time["pontos"], time["vitorias"], time["derrotas"], time["empates"]))
 
-        self.tabela_campeonatos.pack()
+        tela_classificacao = Tela_Classificacao(classificacao_campeonato)
+        tela_classificacao.mainloop()
+
+
 
     def inserir_item_campeonatos(self, tabela, campeonato):
         tabela.insert('', 'end', values=(campeonato["nome"], campeonato["descricao"]))
         
 
     def tabela_selection_campeonato(self):
-        selected_item = self.tabela_campeonatos.selection()[0]
+        selected_item = self.tabela_campeonatos.selection()
         campeonato = self.tabela_campeonatos.item(selected_item, 'values')
         return campeonato
 
@@ -99,7 +97,9 @@ class Tela_Campeonatos(tk.Toplevel):
         print(f"Campeonato selecionado: Nome - {record[0]}, Descrição - {record[1]}")
 
     def ranking(self):
-        tela_classificacao = Tela_Classificacao(self.controller,classificacao)
-        tela_classificacao.mainloop()
-        
-        
+        # Chame construir_tabela_campeonatos para criar a tabela antes de acessá-la
+        self.construir_tabela_campeonatos(classificacao_campeonato)
+        campeonato_selecionado = self.tabela_selection_campeonato()
+        if campeonato_selecionado:
+            tela_classificacao = Tela_Classificacao(self.controller, classificacao_campeonato)
+            tela_classificacao.mainloop()
